9bdfe7ad8153c8dfb15d4fd02f92c72d170d233a
kennyfrc <fxkennyfrc@gmail.com>
2023-12-07 17:45:36 +0800

    feat: alpine.js calculator

---
+++ README.md
@@ -0,0 +1,21 @@
+# Alpine.js Calculator
+
+This project is a simple calculator built using Alpine.js, HTML, CSS, and JavaScript.
+
+## Files
+
+- `index.html`: This is the main HTML file that contains the structure of the calculator.
+- `calculator.js`: This JavaScript file contains the logic for the calculator operations.
+- `styles.css`: This CSS file contains the styles for the calculator.
+
+## Usage
+
+To use this calculator, you can serve the files using a simple HTTP server. For example, if you have Python installed, you can use the following command, using Python's built-in HTTP server:
+
+```bash
+python3 -m http.server 8000
+```
+
+Then, you can open the calculator in your browser by navigating to `http://localhost:8000/`.
+
+Alternatively, you can also use other one-liner HTTP servers through this link: [https://gist.github.com/willurd/5720255](https://gist.github.com/willurd/5720255).
---
+++ calculator.js
@@ -0,0 +1,85 @@
+/*
+ * By: Monolith Growth Consulting
+ * monolithgrowth.com
+ */
+function calculator() {
+    return {
+        currentInput: '',
+        result: '',
+        operation: null,
+        previousKey: '',
+        clearAll() {
+            this.currentInput = '';
+            this.result = '';
+            this.operation = null;
+            this.previousKey = '';
+        },
+        clearEntry() {
+            this.currentInput = '';
+        },
+        inputDigit(digit) {
+            if (this.previousKey === 'operator' || this.previousKey === 'calculate') {
+                this.currentInput = '';
+            }
+            this.currentInput += digit;
+            this.previousKey = 'digit';
+        },
+        inputDecimal() {
+            if (!this.currentInput.includes('.')) {
+                this.currentInput += '.';
+            }
+            this.previousKey = 'decimal';
+        },
+        inputOperation(operator) {
+            if (this.operation && this.previousKey === 'digit') {
+                this.calculate();
+            }
+            this.operation = operator;
+            this.result = this.currentInput;
+            this.previousKey = 'operator';
+        },
+        toggleSign() {
+            this.currentInput = this.currentInput ? String(-parseFloat(this.currentInput)) : '';
+            this.previousKey = 'toggleSign';
+        },
+        calculatePercentage() {
+            if (this.currentInput) {
+                this.currentInput = String(parseFloat(this.currentInput) / 100);
+                this.result = '';
+            }
+            this.previousKey = 'percentage';
+        },
+        calculate() {
+            let calculationResult;
+            const current = parseFloat(this.currentInput);
+            const previous = parseFloat(this.result);
+            if (isNaN(previous) || isNaN(current)) return;
+
+            switch (this.operation) {
+                case '+':
+                    calculationResult = previous + current;
+                    break;
+                case '-':
+                    calculationResult = previous - current;
+                    break;
+                case '*':
+                    calculationResult = previous * current;
+                    break;
+                case '/':
+                    if (current === 0) {
+                        alert("You can't divide by zero.");
+                        return;
+                    }
+                    calculationResult = previous / current;
+                    break;
+                default:
+                    return;
+            }
+
+            this.currentInput = String(calculationResult);
+            this.operation = null;
+            this.result = '';
+            this.previousKey = 'calculate';
+        }
+    };
+}
